ff3f49183ae1cf3c2be3036abfb328537ed9dd7e
Ahmadreza Karim <ahmadrezakarim@gmail.com>
2025-05-22 11:31:24 +0200

    didnt update the url

--- api/login.js
+++ api/login.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://yourusername.github.io');
+    res.setHeader('Access-Control-Allow-Origin', 'https://situatietekening.vercel.app/');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   
