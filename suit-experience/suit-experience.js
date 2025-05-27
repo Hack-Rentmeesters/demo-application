3babbf9226a0de90067e80ef98ab7f6c88b950c7
Ahmadreza Karim <ahmadrezakarim@gmail.com>
2025-05-27 13:42:15 +0200

    Pose.Pose

--- suit-experience/suit-experience.js
+++ suit-experience/suit-experience.js
@@ -8,7 +8,7 @@ const suitImg = new Image();
 suitImg.src = 'images/suit.png'; // suit overlay should be inside /images/
 
 // Initialize pose detection
-const pose = new Pose.Pose({
+const pose = new Pose({
   locateFile: (file) => {
     return `https://cdn.jsdelivr.net/npm/@mediapipe/pose@0.5.1675469404/${file}`;
   }
